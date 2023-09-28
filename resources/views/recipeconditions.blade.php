59f75710bf510e72f521b7adbb0d7326a56f599d
Ryota Hanada <nosebleed4423@gmail.com>
2023-09-28 20:09:14 +0900

    submitダブルクリック防止

--- resources/views/recipeconditions.blade.php
+++ resources/views/recipeconditions.blade.php
@@ -39,5 +39,14 @@
                 <button type="submit"class="button">条件を適用して検索</button>
             </form>
         </div>
+        <script type="text/javascript">
+            //送信ボタンを押した際に送信ボタンを無効化する（連打による多数送信回避）
+            $(function(){
+                $('[type="submit"]').click(function(){
+                    $(this).prop('disabled',true);//ボタンを無効化する
+                    $(this).closest('form').submit();//フォームを送信する
+                });
+            });
+</script>
     </body>
 </html>
